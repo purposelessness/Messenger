ea02d8720e0fa3180475ea6a9f281a1de0724b70
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-02-01 15:03:14 +0300

    refactor: removed old files

--- database/database_service.cc
+++
@@ -1,9 +0,0 @@
-#include "database_service.h"
-
-DatabaseService::DatabaseService(std::string credentials_db_name)
-    : credentials_db_name_(std::move(credentials_db_name)) {}
-
-std::optional<uint64_t> DatabaseService::CheckCredentials(
-    const std::string& login, const std::string& password) {
-  
-}
