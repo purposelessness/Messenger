e36e46aa1f917391f0f78edeaac298d0f00eb0dd
Purposelessness <yagodarov.misha.2003@mail.ru>
2023-02-03 17:36:48 +0300

    feat(client/net): client side net communication

--- client/net/net_service.cc
+++ client/net/net_service.cc
@@ -1,6 +1,7 @@
 #include "net_service.h"
 
 #include <iostream>
+#include <thread>
 
 NetService::NetService(const std::shared_ptr<grpc::Channel>& channel)
     : stub_(messenger::Messenger::NewStub(channel)) {}
@@ -9,6 +10,8 @@ void NetService::Run() {
   is_active_ = true;
   grpc::ClientContext context;
   stream_ = stub_->OpenMessageStream(&context);
+
+  auto read_thread = std::jthread(&NetService::ReadMessages, this);
   while (is_active_) {
     if (bus_.Empty()) {
       std::this_thread::yield();
@@ -16,6 +19,7 @@ void NetService::Run() {
       bus_.WaitAndPop()->Accept(*this);
     }
   }
+
   stream_->WritesDone();
   auto status = stream_->Finish();
   if (!status.ok()) {
@@ -26,9 +30,30 @@ void NetService::Run() {
   LogOut();
 }
 
+void NetService::ReadMessages() {
+  messenger::Message msg;
+  while (stream_->Read(&msg)) {
+    std::cout << "New message!\nSender: " << msg.sender()
+              << "\nChat id: " << msg.chat_id() << "\nData: " << msg.data()
+              << '\n';
+    if (!chats_.contains(msg.chat_id())) {
+      auto chat_sum = GetChatSummary(msg.chat_id());
+      if (!chat_sum.has_value()) {
+        std::cout << "Cannot get chat sum\n";
+        continue;
+      }
+      std::scoped_lock lk(m_);
+      chats_.emplace(msg.chat_id(), chat_sum.value());
+    }
+  }
+}
+
 void NetService::LogOut() {
+  if (!logged_in_) {
+    return;
+  }
   messenger::LogOutRequest request;
-  request.set_login("Me");
+  request.set_id(id_);
   google::protobuf::Empty empty;
 
   grpc::ClientContext context;
@@ -37,25 +62,54 @@ void NetService::LogOut() {
 }
 
 void NetService::Visit(const LoginMessage& message) {
+  if (logged_in_) {
+    std::cout << "Already logged in\n";
+    return;
+  }
   messenger::LogInRequest request;
   request.set_login(message.login);
   request.set_password(message.password);
-  google::protobuf::Empty empty;
+  messenger::LogInResponce response;
 
-  grpc::ClientContext context;
-  grpc::Status status = stub_->LogIn(&context, request, &empty);
-  if (status.ok()) {
-    std::cout << "User logged in\n";
-    return;
+  grpc::ClientContext login_context;
+  if (message.signup) {
+    auto status = stub_->SignUp(&login_context, request, &response);
+    if (!status.ok()) {
+      std::cout << "Cannot sign up: " << status.error_code() << " | "
+                << status.error_message() << '\n';
+      return;
+    }
+    std::cout << "Signed up\n";
+  } else {
+    grpc::Status status = stub_->LogIn(&login_context, request, &response);
+    if (!status.ok()) {
+      std::cout << "LogIn error, code: " << status.error_code()
+                << ", message: " << status.error_message() << '\n';
+      return;
+    }
   }
-  std::cout << "LogIn error, code: " << status.error_code()
-            << ", message: " << status.error_message() << '\n';
+  id_ = response.id();
+
+  messenger::Message init_msg;
+  init_msg.set_sender(id_);
+  stream_->Write(init_msg);
+
+  SetupInfo();
 }
 
 void NetService::Visit(const ChatMessage& message) {
+  if (!logged_in_) {
+    std::cout << "Log in first\n";
+  }
+  std::shared_lock lk(m_);
+  if (!chats_.contains(message.chat_id)) {
+    std::cout << "I don't know this chat\n";
+    return;
+  }
+  lk.unlock();
   messenger::Message msg;
-  msg.set_sender("Me");
-  msg.set_receiver(":(");
+  msg.set_sender(id_);
+  msg.set_chat_id(message.chat_id);
   msg.set_data(message.data);
   stream_->Write(msg);
 }
@@ -65,6 +119,121 @@ void NetService::Visit([[maybe_unused]] const QuitMessage& message) {
   is_active_ = false;
 }
 
+void NetService::Visit([[maybe_unused]] const PrintChatsInfoMessage& message) {
+  if (!logged_in_) {
+    std::cout << "Log in first\n";
+  }
+  std::shared_lock lk(m_);
+  if (chats_.empty()) {
+    std::cout << "No chats.\n";
+    return;
+  }
+  auto func = [this](const auto& p) { PrintChatInfo(p); };
+  std::for_each(chats_.cbegin(), chats_.cend(), func);
+}
+
+void NetService::Visit(const CreateChatMessage& message) {
+  if (!logged_in_) {
+    std::cout << "Log in first\n";
+  }
+  messenger::CreateChatRequest request;
+  std::unordered_set<Id> ids;
+  ids.reserve(message.logins.size());
+  std::for_each(message.logins.cbegin(), message.logins.cend(),
+                [this, &ids](const auto& s) {
+                  auto id = GetUserId(s);
+                  if (id.has_value()) {
+                    ids.emplace(id.value());
+                  }
+                });
+  if (ids.empty()) {
+    std::cout << "Empty list of valid users\n";
+    return;
+  }
+  ids.emplace(id_);
+  request.mutable_users()->Assign(ids.begin(), ids.end());
+  messenger::CreateChatResponse responce;
+  grpc::ClientContext context;
+  auto status = stub_->CreateChat(&context, request, &responce);
+  auto chat_id = responce.chat_id();
+  auto chat_sum = GetChatSummary(chat_id);
+  if (!chat_sum.has_value()) {
+    std::cout << "Cannot create chat.\n";
+    return;
+  }
+  std::cout << "Chat created, id: " << chat_id << '\n';
+  std::unique_lock lk(m_);
+  chats_.emplace(chat_id, std::move(chat_sum.value()));
+}
+
+void NetService::PrintChatInfo(const std::pair<Id, messenger::ChatSummary>& p) {
+  const auto& sum = p.second;
+  std::cout << "Chat " << sum.id() << "\nUsers: ";
+  std::for_each(sum.users().cbegin(), sum.users().cend(), [this](Id id) {
+    auto login = GetUserLogin(id);
+    if (login.has_value()) {
+      std::cout << login.value() << ", ";
+    }
+  });
+  std::cout << '\n';
+}
+
 void NetService::SetOutputBus(NetService::Bus* out_bus) { out_bus_ = out_bus; }
 
 NetService::Bus& NetService::GetBus() { return bus_; }
+
+void NetService::SetupInfo() {
+  google::protobuf::UInt64Value id_msg;
+  id_msg.set_value(id_);
+  messenger::UserSummary sum;
+  grpc::ClientContext sum_context;
+  stub_->GetUserSummary(&sum_context, id_msg, &sum);
+
+  messenger::GetChatsRequest chats_request;
+  chats_request.mutable_chat_ids()->CopyFrom(sum.chat_ids());
+  messenger::GetChatsResponse chats_responce;
+  grpc::ClientContext chats_context;
+  stub_->GetChats(&chats_context, chats_request, &chats_responce);
+  std::for_each(
+      chats_responce.chats().cbegin(), chats_responce.chats().cend(),
+      [&c = chats_](const auto& c_sum) { c.emplace(c_sum.id(), c_sum); });
+  logged_in_ = true;
+  std::cout << "User logged in\n";
+}
+
+std::optional<messenger::ChatSummary> NetService::GetChatSummary(Id chat_id) {
+  messenger::GetChatsRequest chats_request;
+  chats_request.add_chat_ids(chat_id);
+  messenger::GetChatsResponse chats_responce;
+  grpc::ClientContext chats_context;
+  stub_->GetChats(&chats_context, chats_request, &chats_responce);
+  return chats_responce.chats().at(0);
+}
+
+std::optional<NetService::Id> NetService::GetUserId(const std::string& login) {
+  google::protobuf::StringValue login_msg;
+  login_msg.set_value(login);
+  google::protobuf::UInt64Value id_msg;
+  grpc::ClientContext context;
+  auto status = stub_->GetId(&context, login_msg, &id_msg);
+  if (!status.ok()) {
+    std::cout << "Cannot get id. Error code " << status.error_code() << ", "
+              << status.error_message() << '\n';
+    return std::nullopt;
+  }
+  return id_msg.value();
+}
+
+std::optional<std::string> NetService::GetUserLogin(Id id) {
+  google::protobuf::StringValue login_msg;
+  google::protobuf::UInt64Value id_msg;
+  id_msg.set_value(id);
+  grpc::ClientContext context;
+  auto status = stub_->GetLogin(&context, id_msg, &login_msg);
+  if (!status.ok()) {
+    std::cout << "Cannot get login. Error code " << status.error_code() << ", "
+              << status.error_message() << '\n';
+    return std::nullopt;
+  }
+  return login_msg.value();
+}
--- client/net/net_service.h
+++ client/net/net_service.h
@@ -3,13 +3,16 @@
 
 #include <grpcpp/grpcpp.h>
 
+#include <shared_mutex>
+
 #include "../message.h"
 #include "../message_visitor.h"
 #include "../threadsafe/queue.h"
 #include "messenger.grpc.pb.h"
 
-class NetService : public MessageVisitor {
+class NetService : public IMessageVisitor {
   using Bus = Queue<std::unique_ptr<Message>>;
+  using Id = uint64_t;
 
  public:
   explicit NetService(const std::shared_ptr<grpc::Channel>& channel);
@@ -19,12 +22,20 @@ class NetService : public MessageVisitor {
   void Visit(const LoginMessage& message) override;
   void Visit(const ChatMessage& message) override;
   void Visit(const QuitMessage& message) override;
+  void Visit(const PrintChatsInfoMessage& message) override;
+  void Visit(const CreateChatMessage& message) override;
 
   void SetOutputBus(Bus* out_bus);
   Bus& GetBus();
 
  private:
+  void SetupInfo();
   void LogOut();
+  void ReadMessages();
+  void PrintChatInfo(const std::pair<Id, messenger::ChatSummary>& p);
+  std::optional<messenger::ChatSummary> GetChatSummary(Id chat_id);
+  std::optional<Id> GetUserId(const std::string& login);
+  std::optional<std::string> GetUserLogin(Id id);
 
   std::unique_ptr<messenger::Messenger::Stub> stub_;
   std::shared_ptr<
@@ -35,6 +46,11 @@ class NetService : public MessageVisitor {
   Bus* out_bus_ = nullptr;
   Bus bus_;
   bool is_active_ = false;
+  bool logged_in_ = false;
+
+  Id id_ = 0;
+  std::unordered_map<Id, messenger::ChatSummary> chats_;
+  std::shared_mutex m_;
 };
 
 #endif  // MESSENGER_NET_NET_SERVICE_H_
